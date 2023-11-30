0465d170ce9fa65e77b025cabfd247665ac12ad8
Vlascenco Daniel <dvlascenco@vlascenoslaptop.eur.adobe.com>
2023-11-30 17:58:18 +0200

    add comments

--- 2022/rust/day02a/src/main.rs
+++ 2022/rust/day02a/src/main.rs
@@ -1,28 +1,32 @@
 fn get_score(round: &str) -> i32 {
     let opponent: &char = &round.chars().nth(0).unwrap();
     let me: &char = &round.chars().nth(2).unwrap();
+    // Selection Points:
+    // Rock - 1; Paper - 2; Scissors - 3
+    // Outcome Points:
+    // Lose - 0; Draw - 3; Win - 6
     match opponent {
-        'A' => {
+        'A' => { // Rock
             match me {
-                'X' => { 1 + 3 },
-                'Y' => { 2 + 6 },
-                'Z' => { 3 + 0 },
+                'X' => { 1 + 3 }, // Rock - Draw
+                'Y' => { 2 + 6 }, // Paper - Win
+                'Z' => { 3 + 0 }, // Scissors - Lose
                 _ => panic!(),
             }
         },
-        'B' => {
+        'B' => { // Paper
             match me {
-                'X' => { 1 + 0 },
-                'Y' => { 2 + 3 },
-                'Z' => { 3 + 6 },
+                'X' => { 1 + 0 }, // Rock - Lose
+                'Y' => { 2 + 3 }, // Paper - Draw
+                'Z' => { 3 + 6 }, // Scissors - Win
                 _ => panic!(),
             }
         },
-        'C' => {
+        'C' => { // Scissors
             match me {
-                'X' => { 1 + 6 },
-                'Y' => { 2 + 0 },
-                'Z' => { 3 + 3 },
+                'X' => { 1 + 6 }, // Rock - Win
+                'Y' => { 2 + 0 }, // Paper - Lose
+                'Z' => { 3 + 3 }, // Scissors - Draw
                 _ => panic!(),
             }
         },
