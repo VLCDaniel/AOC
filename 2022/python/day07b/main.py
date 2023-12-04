1a9b100f6dceb60d21a26d6febfac32c0a7e682e
Vlascenco Daniel <dvlascenco@vlascenoslaptop.eur.adobe.com>
2023-12-04 16:38:29 +0200

    7-b

---
+++ 2022/python/day07b/main.py
@@ -0,0 +1,21 @@
+import os
+from collections import defaultdict
+from itertools import accumulate
+
+dirs = defaultdict(int)
+f = open(os.path.join(os.path.dirname(__file__), 'input.txt'), "r")
+input_data = f.read().splitlines()
+
+for line in input_data:
+    match line.split():
+        case '$', 'cd', '/': curr = ['/']
+        case '$', 'cd', '..': curr.pop()
+        case '$', 'cd', x: curr.append(x + '/')
+        case '$', 'ls': pass
+        case 'dir', _: pass
+        case size, _:
+            for p in accumulate(curr):
+                dirs[p] += int(size)
+
+print(min(s for s in dirs.values() if s >= dirs['/'] - 40_000_000))
+f.close()
